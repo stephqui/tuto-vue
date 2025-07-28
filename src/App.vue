b6f843e263dea2cdd1a61a2c13b061a44859bfe2
stephqui <paradivuiz@gmail.com>
2025-07-28 22:39:03 +0200

    Syntaxe .vue

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<p>Compteur : {{ count }}</p>
+<button v-on:click="count++">Incrémenter</button>
+<button @click="decrement">Décrémenter</button>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+
+const count = ref(0)
+const increment =(event)=>{
+  count.value++
+}
+const decrement =()=>{
+  count.value--
+}
+</script>
