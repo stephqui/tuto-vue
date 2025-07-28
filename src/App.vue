5126df1dbd56e4cac8575e3c3e7e4c3855c661ae
stephqui <paradivuiz@gmail.com>
2025-07-28 22:54:49 +0200

    Tuto Syntaxe .vue 14'30

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-<p>Compteur : {{ count }}</p>
-<button v-on:click="count++">Incrémenter</button>
-<button @click="decrement">Décrémenter</button>
+  <p>Compteur : {{ count }}</p>
+  <button v-on:click="count++">Incrémenter</button>
+  <button @click="decrement">Décrémenter</button>
+  <ul>
+    <li v-for="movie in movies">
+      {{ movie }} <button @click="deleteMovie(movie)">Supprimer</button>
+    </li>
+  </ul>
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 
 const count = ref(0)
-const increment =(event)=>{
+const movies = ref([
+  'Matrix',
+  'Lilo & Stitch',
+  'Titanic'
+])
+
+const increment = (event) => {
   count.value++
 }
-const decrement =()=>{
+const decrement = () => {
   count.value--
+}
+
+const deleteMovie = (movie) => {
+  movies.value = movies.value.filter(m => m != movie)
 }
 </script>
